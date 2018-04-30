d4c0ba7a3b4d9f48e087862aff8ae254aa1bbfc5
Sorumi <Sorumi33@gmail.com>
2018-04-30 11:12:43 +0800

    init

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+
+	float maxX = 5.0f;
+	float maxZ = 5.0f;
+
+	float yAxis = 0.5f;
+
+	void OnMouseDrag() {
+		
+		Plane plane = new Plane(Vector3.down, yAxis);
+    	Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    	float distance;
+    	if(plane.Raycast(ray, out distance)) {
+			Vector3 mousePosition = ray.GetPoint(distance);
+			float x = Mathf.Clamp(Mathf.Round(mousePosition.x - 0.5f), 0.0f, maxX) + 0.5f;
+			float z = Mathf.Clamp(Mathf.Round(mousePosition.z - 0.5f), 0.0f, maxX) + 0.5f;
+			Vector3 objPosition = new Vector3(x, yAxis, z);
+       		transform.position = objPosition;
+    	}
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
