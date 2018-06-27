b52416ca6f7f4d9de9a70bbd5a8faffec2d233a8
Sorumi <Sorumi33@gmail.com>
2018-06-27 22:11:21 +0800

    dragable item in scroll rect

--- Assets/Scripts/UI/ButtonCellView.cs
+++ Assets/Scripts/UI/ButtonCellView.cs
@@ -2,30 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using Sorumi.UI;
 
 [RequireComponent(typeof(CellView))]
 public class ButtonCellView : MonoBehaviour {
 
-	// Use this for initialization
-	public int count;
-	private CellView cellView;
+    // Use this for initialization
+    public int count;
+    private CellView cellView;
 
-	void Start () {
-		cellView = GetComponent<CellView>();
-		cellView.CountOfCell = CountOfCell;
-		cellView.CellAtIndex = CellAtIndex;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	private ScrollRect scrollRect;
+    void Start()
+    {
+		scrollRect = GetComponent<ScrollRect>();
 
-	void CellAtIndex(GameObject gameObject, int index) {
-		gameObject.transform.Find("Text").GetComponent<Text>().text = index.ToString();
-	}
-	int CountOfCell() {
-		return count;
-	}
+        cellView = GetComponent<CellView>();
+        cellView.CountOfCell = CountOfCell;
+        cellView.CellAtIndex = CellAtIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetMouseButton(0))
+        //  {
+        //      //Get the mouse position on the screen and send a raycast into the game world from that position.
+        //      Vector2 worldPoint = camera.ScreenToWorldPoint(Input.mousePosition);
+        //      RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
+
+        // 	 Debug.Log(1);
+
+        //      //If something was hit, the RaycastHit2D.collider will not be null and the the object must have the "Monkey" tag and target has to be null
+        //     //  if (hit.collider != null && hit.collider.tag == "Monkey" && target == null)
+        //     //  {
+        //     //      target = hit.collider.transform; // Sets the target to be the transform that was hit
+        //     //  }
+
+        //     //  if (target != null)
+        //     //  {
+        //     //      target.position = worldPoint; // Moves target with the mouse
+        //     //  }
+        //  }
+
+        //  if (Input.GetMouseButtonUp(0))
+        //  {
+        //     //  target = null; // Sets the target to null again
+        // 	Debug.Log(2);
+        //  }
+    
+    }
+
+    void CellAtIndex(GameObject gameObject, int index)
+    {
+		// gameObject.GetComponent<ItemButton>().parentScroll = scrollRect;
+        gameObject.transform.Find("Text").GetComponent<Text>().text = index.ToString();
+    }
+    int CountOfCell()
+    {
+        return count;
+    }
 }
---
+++ Assets/Scripts/UI/ItemButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ItemButton : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public ScrollRect parentScroll;
+
+    private bool scroll;
+
+    void Start () 
+    {
+        parentScroll = GetComponentInParent<ScrollRect> ();
+    }
+ 	public void OnBeginDrag(PointerEventData eventData)
+     {
+	// 	Debug.Log("Press position + " + eventData.pressPosition);
+	// 	Debug.Log("End position + " + eventData.position);
+
+		float diffX = Mathf.Abs(eventData.position.x - eventData.pressPosition.x);
+		float diffY = Mathf.Abs(eventData.position.y - eventData.pressPosition.y);
+		if (diffX < diffY){
+            Debug.Log(1);
+			scroll = true;
+            parentScroll.OnBeginDrag(eventData);
+		} else {
+            scroll = false;
+        }
+     }
+ 
+     public void OnDrag(PointerEventData eventData)
+     {
+        if (scroll) {
+            parentScroll.OnDrag(eventData);
+        }
+     }
+ 
+     public void OnEndDrag(PointerEventData eventData)
+     {
+		 if (scroll) {
+            parentScroll.OnEndDrag(eventData);
+        }
+     }
+}
