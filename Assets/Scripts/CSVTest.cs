741190915ba9659132414e68451dd7d3dc0b5d4e
Sorumi <Sorumi33@gmail.com>
2018-08-12 16:56:12 +0800

    csv helper and test

---
+++ Assets/Scripts/CSVTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sorumi.Util;
+
+public class CSVTest : MonoBehaviour {
+
+        public TextAsset csvFile;
+        public void Start()
+        {
+            string[,] csvArray = CSVHelper.SplitCsv(csvFile.text);
+            Debug.Log("size = " + csvArray.GetLength(0) + "," + csvArray.GetLength(1));
+
+            Debug.Log(CSVHelper.ArrayToString(csvArray));
+        }
+
+}
---
+++ Assets/Scripts/Framework/Util/CSVHelper.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Sorumi.Util
+{
+    public class CSVHelper
+    {
+        public TextAsset csvFile;
+        public void Start()
+        {
+            string[,] csvArray = SplitCsv(csvFile.text);
+            Debug.Log("size = " + csvArray.GetLength(0) + "," + csvArray.GetLength(1));
+
+            Debug.Log(ArrayToString(csvArray));
+        }
+
+        // outputs the content of a 2D array, useful for checking the importer
+        public static string ArrayToString(string[,] array)
+        {
+            StringBuilder str = new StringBuilder("");
+            for (int x = 0; x < array.GetLength(0); x++)
+            {
+                for (int y = 0; y < array.GetLength(1); y++)
+                {
+                    str.Append(array[x, y] + "|");
+                }
+                str.Append("\n");
+            }
+            return str.ToString();
+        }
+
+        public static string[,] SplitCsv(string text)
+        {
+            string[] rows = text.Split('\n');
+            int countOfRow = rows.Length;
+            int countOfCol = SplitLine(rows[0]).Length;
+
+            string[,] result = new string[countOfRow, countOfCol];
+            for (int x = 0; x < countOfRow; x++)
+            {
+                string[] row = SplitLine(rows[x]);
+                for (int y = 0; y < countOfCol; y++)
+                {
+                    result[x, y] = row[y];
+                    // result[x, y] = result[x, y];
+                }
+            }
+
+            return result;
+        }
+
+        // splits a CSV row 
+        public static string[] SplitLine(string line)
+        {
+            string[] arrays = line.Split(';');
+            return arrays;
+        }
+    }
+}
